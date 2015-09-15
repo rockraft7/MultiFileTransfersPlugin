543db05ec7ee7963b462645e5bad9eab3a3eda6b
Faizal Sidek <faizalsidek@gmail.com>
2015-09-16 01:13:36 +0800

    fix feature etc

--- src/android/MultiFileTransfers.java
+++ src/android/MultiFileTransfers.java
@@ -1,5 +1,25 @@
 package com.rockraft7.plugin.multifiletransfers;
 
-public class MultiFileTransfers {
+import android.widget.Toast;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
+import org.json.JSONArray;
+import org.json.JSONException;
 
+public class MultiFileTransfers extends CordovaPlugin {
+    public static final String TAG = MultiFileTransfers.class.getName();
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if ("upload".equals(action)) {
+            String files = args.getString(0);
+            String params = args.getString(1);
+
+            LOG.d(TAG, "Files=" + files + ", Params=" + params);
+            Toast.makeText(cordova.getActivity(), "Hello!!!", Toast.LENGTH_LONG).show();
+        }
+
+        return false;
+    }
 }
--- www/multifiletransfers.js
+++ www/multifiletransfers.js
@@ -1,5 +1,7 @@
-var exec = require('cordova/exec');
+cordova.define("cordova-plugin-multifiletransfer.MultiFileTransfers", function (require, exports, module) {
+    var exec = require('cordova/exec');
 
-exports.uploadmultiple = function (files, params, success, error) {
-    exec(success, error, "MultipleFileTransfer", "upload", [files, params]);
-};
+    exports.uploadmultiple = function (files, params, success, error) {
+        exec(success, error, "MultiFileTransfers", "upload", [files, params]);
+    };
+});
